f18af5bcd19c2b7126898509e37437c5493be7f7
Michael Nguyen <chessdork@gmail.com>
2015-03-29 22:08:21 -0500

    Updated .gitignore, added braille constants.

---
+++ src/com/github/chessdork/braille/util/BrailleConstants.java
@@ -0,0 +1,6 @@
+package com.github.chessdork.braille.util;
+
+public class BrailleConstants {
+	public static final String CAPITAL_SIGN = "\u2820";
+	public static final String NUMERAL_SIGN = "\u283C";
+}
