9093f0669e8ed60e6ee7907caecc3c203feda3eb
Michael Nguyen <chessdork@gmail.com>
2015-03-31 16:07:47 -0500

    Updated BrailleLexer and added more unit tests.
    BrailleLexer State determines whether characters are automatically
    appended to the token.  A queue allows for back-tracking.

--- src/main/java/com/github/chessdork/braille/lang/BrailleLexer.java
+++ src/main/java/com/github/chessdork/braille/lang/BrailleLexer.java
@@ -3,12 +3,15 @@ package com.github.chessdork.braille.lang;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BrailleLexer {
 	private String input;
 	private int index;
 	private State state;
+	private Queue<Character> queue;
 	
 	public BrailleLexer(String input) {
 		if (input == null) {
@@ -16,10 +19,11 @@ public class BrailleLexer {
 		}
 		this.input = input;
 		this.index = 0;
+		this.queue = new LinkedList<>();
 	}
 	
 	public String nextToken() {
-		if (index >= input.length()) {
+		if (index >= input.length() && queue.isEmpty()) {
 			return null;
 		}
 		String token = "";
@@ -27,11 +31,25 @@ public class BrailleLexer {
 		
 		setState(State.INITIAL);
 		
+		while (!queue.isEmpty() && state != State.TOKEN_COMPLETED) {
+			c = queue.poll();
+			setState(state.nextState(c));
+			token += c;
+		}
+		
 		while (state != State.TOKEN_COMPLETED && index < input.length()) {
 			c = input.charAt(index++);
-			token += c;
 			setState(state.nextState(c));
+			if (state.isAutoAppend()) {
+				while (!queue.isEmpty()) {
+					token += queue.poll();
+				}
+				token += c;
+			} else {
+				queue.add(c);
+			}
 		}
+		System.out.println(token);
 		return token;
 	}
 	
@@ -40,36 +58,59 @@ public class BrailleLexer {
 	}
 	
 	protected enum State {
-		INITIAL {
+		INITIAL(true) {
 			@Override
 			public State nextState(char c) {
 				if (Character.isDigit(c)) {
 					return NUMERIC;
+				} else if (c == '.' || c == ',') {
+					return NUMERIC_INDETERMINATE;
+				} else if (c == ' ') {
+					return TOKEN_COMPLETED;
 				} else {
 					throw new IllegalStateException();
 				}
 			}
 		},
-		NUMERIC {
+		NUMERIC(true) {
 			@Override
 			public State nextState(char c) {
 				if (Character.isDigit(c) || c == ',' || c == '.') {
 					return NUMERIC;
-				} 
-				else {
+				} else if (c == ' ') {
+					return NUMERIC_INDETERMINATE;
+				} else {
 					return TOKEN_COMPLETED;
 				}
 			}
 		},
-		TOKEN_COMPLETED {
+		NUMERIC_INDETERMINATE(false) {
+			@Override
+			public State nextState(char c) {
+				if (Character.isDigit(c)) {
+					return NUMERIC;
+				} else {
+					return TOKEN_COMPLETED;
+				}
+			}
+		},
+		TOKEN_COMPLETED(false) {
 			@Override
 			public State nextState(char c) {
 				return INITIAL;
 			}
 		};
 		
-		public abstract State nextState(char c);
-		//public abstract String process(String input, int index);
-	}
+		private final boolean autoAppend;
+		
+		State(boolean autoAppend) {
+			this.autoAppend = autoAppend;
+		}
 
+		public boolean isAutoAppend() {
+			return autoAppend;
+		}
+		
+		public abstract State nextState(char c);
+	}
 }
--- src/test/java/com/github/chessdork/braille/lang/BrailleLexerTest.java
+++ src/test/java/com/github/chessdork/braille/lang/BrailleLexerTest.java
@@ -44,10 +44,61 @@ public class BrailleLexerTest {
 	}
 	
 	@Test
+	public void testUEB6() {
+		BrailleLexer lexer = new BrailleLexer("0.7");
+		String token = lexer.nextToken();
+		assertEquals(token, "0.7");
+	}
+	
+	@Test
+	public void testUEB7() {
+		BrailleLexer lexer = new BrailleLexer("8,93");
+		String token = lexer.nextToken();
+		assertEquals(token, "8,93");
+	}
+
+	@Test
+	public void testUEB8() {
+		BrailleLexer lexer = new BrailleLexer(",7");
+		String token = lexer.nextToken();
+		assertEquals(token, ",7");
+	}
+	
+	@Test
+	public void testUEB9() {
+		BrailleLexer lexer = new BrailleLexer("0,7");
+		String token = lexer.nextToken();
+		assertEquals(token, "0,7");
+	}
+	
+	@Test
+	public void testUEB10() {
+		BrailleLexer lexer = new BrailleLexer("4.2.2");
+		String token = lexer.nextToken();
+		assertEquals(token, "4.2.2");
+	}
+	
+	@Test
+	public void testUEB11() {
+		BrailleLexer lexer = new BrailleLexer("4 500 000");
+		String token = lexer.nextToken();
+		assertEquals(token, "4 500 000");
+	}
+	
+	@Test
 	public void testTrailingSpaces() {
 		BrailleLexer lexer = new BrailleLexer("62    ");
 		String token = lexer.nextToken();
 		assertEquals(token, "62");
+	}
+	
+	@Test
+	public void testMultiTokenInput() {
+		BrailleLexer lexer = new BrailleLexer("62 a");
+		String token = lexer.nextToken();
+		String token2 = lexer.nextToken();
+		System.out.println("TOKEN 2: " + token2);
+		assertArrayEquals(new String[] {token, token2}, new String [] {"62", " "});
 	}
 	
 	@Test
